9b75d6ed9c5c392c54dc1c8a0bb33107abd4316e
Peterturn <55192090+Peterturn@users.noreply.github.com>
2021-06-12 11:34:25 +0200

    basic file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ApikeyComponent } from './apikey/apikey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    MainPageComponent,
+    ApikeyComponent
   ],
   imports: [
     BrowserModule,
